99bca3d0ca1cbcf18488b423f340488e261f8b69
ahmedragab <ahmedragab20901@gmail.com>
2023-08-24 23:26:11 +0300

    hands on

---
+++ app.v2/src/App.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+  data() {
+    return {
+      headline: 1,
+    };
+  },
+  render(h) {
+    return /* returns vNode tree */ h(
+      "div",
+      {
+        attrs: {
+          id: "app",
+        },
+      },
+      [
+        h(
+          `h${this.headline}`,
+          {
+            style: {
+              color: "indigo",
+              fontSize: "50px",
+            },
+          },
+          "Hello World!"
+        ),
+        h(
+          "p",
+          {
+            on: {
+              click: () => {
+                alert("Hello World!");
+              },
+            },
+          },
+          "Click me!"
+        ),
+        h("p", {
+          domProps: {
+            innerHTML: "Hello World!",
+          },
+        }),
+      ]
+    );
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app.v2/src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => {
+    return h(App);
+  },
+}).$mount("#app");
+
+console.log(
+  new Vue({
+    render: (h) => {
+      return h(App);
+    },
+  }).$mount
+);
